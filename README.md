4ffcd24be197ad7d90b5a646791cba4c61016562
Samuel Williams <samuelhwilliams@gmail.com>
2018-08-25 13:47:41 +0100

    Use Yarn rather than NPM

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 WIP Github<->Trello for Product Acceptance
+
+### Developer machine dependencies
+
+* Node 10.4.0
+* Yarn (v1.9.4)
--- app/factory.py
+++ app/factory.py
@@ -13,14 +13,12 @@ from app.errors import TrelloUnauthorized, GithubUnauthorized
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
-    app.config.from_object(config_map[os.environ.get("FLASK_ENV", "development")])
+    app.config.from_object(config_map[os.environ.get("FLASK_ENV", "production")])
 
     db.init_app(app)
-
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.login_view = ".index"
-
-    migrate.init_app(app, db)
 
     app.register_blueprint(main_blueprint)
 
